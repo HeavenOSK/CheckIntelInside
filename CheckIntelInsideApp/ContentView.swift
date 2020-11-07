74a960fc3a4cb1f22df5e7baf81fdd47c1894776
heavenOSK <heavenosk@gmail.com>
2020-11-07 13:42:20 +0900

    Initial Commit

---
+++ CheckIntelInsideApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CheckIntelInsideApp
+//
+//  Created by 渡邉龍之介 on 2020/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
